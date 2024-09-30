30f6284d28f89b0d98396d552ffdf05bb4242a10
MudasirBhat <bhatmuddu471@gmail.com>
2024-09-30 23:45:29 +0530

    making paralex effect

--- ZakatCare/env.js
+++ ZakatCare/env.js
@@ -1,4 +1,4 @@
 const LOCAL_HOST = 'http://localhost:8090'
 // const LOCAL_HOST = 'https://wanderlust-backend-qe8j.onrender.com'
 // const LOCAL_HOST = 'https://wanderlust-backend-1.onrender.com'
-export default LOCAL_HOST;
+export default LOCAL_HOST;;
