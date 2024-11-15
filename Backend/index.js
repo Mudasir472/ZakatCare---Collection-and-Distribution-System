5313c071789c9b613dd204c2125553d301a8b9d4
MudasirBhat <bhatmuddu471@gmail.com>
2024-11-15 23:51:32 +0530

    adding vercel.json in backend

--- Backend/index.js
+++ Backend/index.js
@@ -28,8 +28,11 @@ const reciever = require("./routes/reciever.router")
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true // Allow credentials (cookies)
+    origin: ["http://localhost:5173", "https://zakat-care.vercel.app"],
+    methods: ["GET", "HEAD", "PUT", "PATCH", "POST", "DELETE", "OPTIONS"],
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    credentials: true,
 }));
 
 app.use(express.json());
