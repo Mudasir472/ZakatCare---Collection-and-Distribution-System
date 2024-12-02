3ae75596c4ae5a5431c4c1bc3dee3585dc391d50
MudasirBhat <bhatmuddu471@gmail.com>
2024-12-02 10:54:26 +0530

    authentication issue solves

--- Backend/routes/auth.router.js
+++ Backend/routes/auth.router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/auth.controller')
 
-// Forgot Password Route
+// Forgot Password Rout
 
 router.post('/forgot-password', authController.forgetPassword );
 
